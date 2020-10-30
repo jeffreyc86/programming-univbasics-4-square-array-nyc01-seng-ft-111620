2ff64dee575b64a3837213685c239f29098cd303
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-30 02:35:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,11 +2,11 @@
 def square_array(array)
   squares = []
   counter = 0
-  while counter >= 0 do
+  while array[counter] do
     squares.push (array[counter] ** 2) 
     counter += 1
   end
-  squares
+  square
 end
 
 
